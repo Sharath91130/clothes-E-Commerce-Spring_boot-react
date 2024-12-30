76bf5809a3fb624578e008fa6092c56eaec80c1c
Sharath91130 <sharathkurer8055@gmail.com>
2024-12-30 21:31:04 +0530

    First commit

---
+++ Sales_Backend/src/main/java/com/kodnest/sales_backend/Controller/UserController.java
@@ -0,0 +1,33 @@
+package com.kodnest.sales_backend.Controller;
+
+import com.kodnest.sales_backend.Enitity.User;
+import com.kodnest.sales_backend.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import java.util.Map;
+
+@RestController
+@CrossOrigin("http://localhost:5173")
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        try {
+            User registeredUser = userService.registerUser(user);
+            return ResponseEntity.ok(Map.of("message", "User registered successfully", "user", registeredUser));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        }
+    }
+}
